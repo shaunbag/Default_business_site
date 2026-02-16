3314894cb140ead8f1ece5c158b87d99f3bc7ebc
ShaunMcelhinney <shaunbag@hotmail.com>
2026-02-16 15:38:28 +0000

    Price Search, updated html and styling for cart and cards

--- src/shopComponents/Cart.tsx
+++ src/shopComponents/Cart.tsx
@@ -3,24 +3,40 @@ import { useShopStore } from "../store";
 
 export default function Cart() {
 
-    const { cart } = useShopStore();    
+    const { cart, removeFromCart } = useShopStore();    
 
+    function handleRemoveFromCart(id: number){
+        removeFromCart(id);
+    }
 
     return(
         <section className="cart-container">
             <NavBar shopNav={true} />
             <h2>My Cart</h2>
-            <ul>
-                {
-                    cart.map(item => {
-                        return (
-                            <li key={item.id}>
-                                <h3>{item.title} ${item.price}</h3>
-                            </li>
-                        )
-                    })
-                }
-            </ul>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Item</th>
+                        <th>Price</th>
+                        <th>Remove</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        cart.map(item => {
+                            return (
+                                <tr key={item.id}>
+                                    <td>{item.title}</td>
+                                    <td>${item.price}</td>
+                                    <td><button onClick={() => handleRemoveFromCart(item.id)}>Remove</button></td>
+                                </tr>
+                            )
+                        }
+                    )
+                    }
+                    <tr><td colSpan={3}>Total: ${cart.reduce((sum, item) => sum + item.price, 0)}</td></tr>
+                </tbody>
+            </table>
         </section>
     )
 }
